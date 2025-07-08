c7be1d2a5dcc22d0f007e2bd97efcf4f8a22332b
Li Xilin <lixilin@gmx.com>
2025-07-08 16:59:04 +0800

    chore: remove files generated by autogen.sh from dist

--- clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -x
-
-make distclean
-rm -rf autom4te.cache build-aux m4 libx/.deps libx/.libs samples/.deps
-rm -f Makefile.in aclocal.m4 config.h.in config.h.in~ configure \
-	configure~ config.h config.status config.log libtool \
-	stamp-h1 libx.pc include/Makefile.in src/Makefile.in
-[ ! -f configure.ac ] && rmdir include libx samples
